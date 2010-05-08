32d867553d2e160f5f87693252f71b22912e1299
de.abg.reichert.joerg <de.abg.reichert.joerg@b0397ec9-f3ff-9c61-4de9-3e5b0ee3f2f0>
2010-05-08 07:35:20 +0000

    examples to exercise the test suite generator for junit4 and compare it to the junit3 test suite generator

---
+++ trunk/junit4runner/example/de.abg.jreichert.junit4runner.example/src/de/abg/jreichert/junit4runner/Divider.java
@@ -0,0 +1,10 @@
+package de.abg.jreichert.junit4runner;
+
+import java.math.BigDecimal;
+
+public class Divider {
+	
+	public BigDecimal divide(BigDecimal divident, BigDecimal divisor) {
+		return divident.divide(divisor);
+	}
+}
