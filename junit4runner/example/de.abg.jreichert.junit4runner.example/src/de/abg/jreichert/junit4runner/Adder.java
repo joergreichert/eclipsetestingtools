af13ac9b031777c21640c079d26a6f4e4e614cb0
de.abg.reichert.joerg <de.abg.reichert.joerg@b0397ec9-f3ff-9c61-4de9-3e5b0ee3f2f0>
2010-05-08 07:35:20 +0000

    examples to exercise the test suite generator for junit4 and compare it to the junit3 test suite generator

---
+++ junit4runner/example/de.abg.jreichert.junit4runner.example/src/de/abg/jreichert/junit4runner/Adder.java
@@ -0,0 +1,10 @@
+package de.abg.jreichert.junit4runner;
+
+import java.math.BigDecimal;
+
+public class Adder {
+	
+	public BigDecimal add(BigDecimal augend1, BigDecimal augend2) {
+		return augend1.add(augend2);
+	}
+}
---
+++ junit4runner/example/de.abg.jreichert.junit4runner.example/src/de/abg/jreichert/junit4runner/Subtractor.java
@@ -0,0 +1,10 @@
+package de.abg.jreichert.junit4runner;
+
+import java.math.BigDecimal;
+
+public class Subtractor {
+
+	public BigDecimal subtract(BigDecimal minuend, BigDecimal subtrahend) {
+		return minuend.subtract(subtrahend);
+	}
+}
