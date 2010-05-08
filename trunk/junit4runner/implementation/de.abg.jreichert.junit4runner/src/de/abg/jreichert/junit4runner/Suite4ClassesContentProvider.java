4c9d883915948c98060f06785637110774ef6957
de.abg.reichert.joerg <de.abg.reichert.joerg@b0397ec9-f3ff-9c61-4de9-3e5b0ee3f2f0>
2010-05-08 07:32:29 +0000

    initial version of the junit 4 test suite generator

---
+++ trunk/junit4runner/implementation/de.abg.jreichert.junit4runner/src/de/abg/jreichert/junit4runner/Suite4ClassesContentProvider.java
@@ -0,0 +1,41 @@
+package de.abg.jreichert.junit4runner;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.internal.junit.launcher.JUnit4TestFinder;
+import org.eclipse.jdt.internal.junit.ui.JUnitPlugin;
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+public class Suite4ClassesContentProvider implements IStructuredContentProvider {
+
+	public Object[] getElements(Object parent) {
+		if (! (parent instanceof IPackageFragment))
+			return new Object[0];
+		IPackageFragment pack= (IPackageFragment) parent;
+		if (! pack.exists())
+			return new Object[0];
+		return getTests(pack).toArray();
+	}
+
+	public Set getTests(IPackageFragment pack) {
+		try {
+			HashSet result= new HashSet();
+			new JUnit4TestFinder().findTestsInContainer(pack, result, null);
+			return result;
+		} catch (CoreException e) {
+			JUnitPlugin.log(e);
+			return Collections.EMPTY_SET;
+		}
+	}
+
+	public void dispose() {
+	}
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	}
+}
